83888eb34646b90e5b2daa6b9a80b8f4aefb4307
Bradley Ringel <brad.ringel@gmail.com>
2014-06-16 22:24:42 -0500

    title cell with image view for poster

---
+++ Visor/VSRTitleCell.h
@@ -0,0 +1,16 @@
+//
+//  VSRTitleCell.h
+//  Visor
+//
+//  Created by Bradley Ringel on 6/16/14.
+//  Copyright (c) 2014 Bradley Ringel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSRTitleCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UIImageView *posterImageView;
+@property (nonatomic, weak) IBOutlet UILabel *titleLabel;
+
+@end
---
+++ Visor/VSRTitleCell.m
@@ -0,0 +1,34 @@
+//
+//  VSRTitleCell.m
+//  Visor
+//
+//  Created by Bradley Ringel on 6/16/14.
+//  Copyright (c) 2014 Bradley Ringel. All rights reserved.
+//
+
+#import "VSRTitleCell.h"
+
+@implementation VSRTitleCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
